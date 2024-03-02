8585acb0dc5da7e29106031e5a5bb781e8acfa70
vishalkrishnads <321vishalds@gmail.com>
2024-03-02 20:19:51 +0530

    Fixed social media previews issue

--- src/components/InitialLoader.jsx
+++ src/components/InitialLoader.jsx
@@ -1,6 +1,9 @@
+import Title from "./Head";
+
 export default function InitialLoader() {
   return (
     <div className="loader w-screen h-screen bg-black z-[28]  flex items-center justify-center">
+      <Title title={"Yukthi '24"} description={"Solve. Create. Thrive."} route={'/'} />
       <video
         src="/preloader.mp4"
         autoPlay
