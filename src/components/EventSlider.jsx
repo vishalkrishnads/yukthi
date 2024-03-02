45cff41ff102c17dcd448f784c3f2272f4474292
Pranav Jerry <libreinator@disroot.org>
2024-03-03 00:13:45 +0530

    made events slider point to events page
    Feedback from Karthik M, S8 CSE

--- src/components/EventSlider.jsx
+++ src/components/EventSlider.jsx
@@ -2,22 +2,21 @@ import React, { useEffect } from "react";
 import Image from "next/image";
 
 export default function EventSlider() {
-  
   const shades = [
     {
-      'label': 'Hackathon',
-      'poster': '/hackathon.png'
+      label: "Hackathon",
+      poster: "/hackathon.png",
     },
     {
-      'label': 'Games',
-      'poster': '/games.png'
+      label: "Games",
+      poster: "/games.png",
     },
     {
-      'label': 'Workshops',
-      'poster': '/workshop.png'
-    }
-  ]
-  
+      label: "Workshops",
+      poster: "/workshop.png",
+    },
+  ];
+
   useEffect(() => {
     const script = document.createElement("script");
     script.src =
@@ -33,24 +32,29 @@ export default function EventSlider() {
       </h1>
       <div className="flex flex-wrap gap-8 md:gap-16 lg:gap-20 pt-10 items-center justify-center px-10 mt-10">
         {shades.map((item, index) => {
-          return <div key={index} data-tilt className="w-fit relative scale-105">
-            <div className="updown">
-              <div className="flex items-center justify-center w-full h-[22rem] xl:h-[30rem] rounded-sm bg-main_primary/50 backdrop:blur-xl hover:-skew-x-6 skew-y-3 transition-all duration-300 ease-in-out">
-                <div className="w-full h-[22rem] xl:h-[30rem] rounded-sm  bg-gray hover:skew-x-6 hover:scale-105 transition-all duration-300 ease-in-out">
-                  <span className="text-3xl font-clash font-semibold px-4 py-2 absolute left-[-2rem] top-4 bg-main_primary text-white">
-                    {item.label.toUpperCase()}
-                  </span>
-                  <Image
-                    src={item.poster}
-                    alt={item.label}
-                    width={500}
-                    height={500}
-                    className="w-full h-full object-cover"
-                  />
+          return (
+            <div key={index} data-tilt className="w-fit relative scale-105">
+              <div className="updown">
+                <div className="flex items-center justify-center w-full h-[22rem] xl:h-[30rem] rounded-sm bg-main_primary/50 backdrop:blur-xl hover:-skew-x-6 skew-y-3 transition-all duration-300 ease-in-out">
+                  <Link
+                    href={`/events`}
+                    className="w-full h-[22rem] xl:h-[30rem] rounded-sm  bg-gray hover:skew-x-6 hover:scale-105 transition-all duration-300 ease-in-out"
+                  >
+                    <span className="text-3xl font-clash font-semibold px-4 py-2 absolute left-[-2rem] top-4 bg-main_primary text-white">
+                      {item.label.toUpperCase()}
+                    </span>
+                    <Image
+                      src={item.poster}
+                      alt={item.label}
+                      width={500}
+                      height={500}
+                      className="w-full h-full object-cover"
+                    />
+                  </Link>
                 </div>
               </div>
             </div>
-          </div>
+          );
         })}
 
         {/* <div data-tilt className="w-fit relative scale-105">
@@ -71,7 +75,6 @@ export default function EventSlider() {
             </div>
           </div>
         </div> */}
-
       </div>
     </div>
   );
